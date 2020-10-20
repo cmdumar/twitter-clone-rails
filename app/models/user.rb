e1e5ebf90a7e09bf340d7f099faa4d0189c7eaee
mohammadumar28 <mohammadumar28@gmail.com>
2020-10-20 21:08:38 +0530

    Implement the follow feature for the users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,23 +6,23 @@ class User < ApplicationRecord
   attr_writer :login
   validate :validate_username
 
-  has_many :active_relationships, class_name: "Follower", foreign_key: "following_id", dependent: :destroy
-  has_many :passive_relationships, class_name: "Follower", foreign_key: "follower_id", dependent: :destroy
+  has_many :active_relationships, class_name: "Following", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Following", foreign_key: "followed_id", dependent: :destroy
 
-  has_many :followings, through: :active_relationships, source: :following
+  has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
-  # Follow helpers
+  # Following helper methods
   def follow(other)
-    active_relationships.create(follower_id: other.id)
+    active_relationships.create(followed_id: other.id)
   end
 
   def unfollow(other)
-    active_relationships.find_by(follower_id: other.id).destroy
+    active_relationships.find_by(followed_id: other.id).destroy
   end
 
   def following?(other)
-    following.include?(other.id)
+    following.include?(other)
   end
 
   # Devise helper methods
