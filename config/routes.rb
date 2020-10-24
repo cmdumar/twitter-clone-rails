01d58fbf9bf3f6ff0fff1f5c0bbc56947072b6e4
mohammadumar28 <mohammadumar28@gmail.com>
2020-10-24 17:52:43 +0530

    Design the sign in and sign up pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :following, only: [:create, :destroy]
 
   get 'users/:id' => 'users#show'
+  get 'users' => 'users#index'
 
   root 'opinions#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
