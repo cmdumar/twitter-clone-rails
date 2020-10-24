9db4e3385e8c74c1b40db3780f99189b966f8def
mohammadumar28 <mohammadumar28@gmail.com>
2020-10-24 16:37:30 +0530

    Implement helper methods for images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,20 @@ module ApplicationHelper
 
     output
   end
+
+  def profile_img(img, options = {})
+    if img.attached?
+      image_tag(img, class: options[:class], alt: "Profile Picture")
+    else
+      image_tag('profile-placeholder.jpg', class: options[:class], alt: "Profile Picture")
+    end
+  end
+
+  def cover_img(img, options = {})
+    if img.attached?
+      content_tag(:div, nil, class: options[:class], style: "background-image: url(#{url_for(img)});")
+    else
+      content_tag(:div, nil, class: options[:class], style: "background-image: url(https://fooddiversity.today/wp-content/uploads/2016/05/placeholder.png);")
+    end
+  end
 end
